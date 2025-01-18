990d651e7a321f6e9a1652bcadb99a88e827525c
Leonardo Alarcon <alarcon842@hotmail.com>
2025-01-18 12:24:45 -0600

    Creacion de Token y seguridad

---
+++ src/main/java/api/foro/hub/api/foro/hub/domain/usuarios/DatosAutenticacionUsuario.java
@@ -0,0 +1,4 @@
+package api.foro.hub.api.foro.hub.domain.usuarios;
+
+public record DatosAutenticacionUsuario(String login, String clave) {
+}
---
+++ src/main/java/api/foro/hub/api/foro/hub/infra/security/DatosJWTToken.java
@@ -0,0 +1,4 @@
+package api.foro.hub.api.foro.hub.infra.security;
+
+public record DatosJWTToken(String jwTtoken) {
+}
